61feb337b1eb637446528d609351e036d7102ea5
preema24 <preema@mangaloreinfotech.in>
2016-03-10 17:59:39 +0530

    Update assignment2.py

--- assignment2.py
+++ assignment2.py
@@ -7,16 +7,21 @@ import sys
 
 connection = MySQLdb.connect(host='localhost', port=3306, user='codaxtr_user', passwd='c0d@xtr', db='codaxtr')
 cursor = connection.cursor()
-cursor.execute("select p.id, p.fullname, u.id,u.unresolved_party_type_name from party p inner join unresolved_party_type u on u.id=p.id order by p.id limit 100;")
+cursor.execute("select p.id, p.fullname, u.id,u.unresolved_party_type_name from party p inner join unresolved_party_type u on u.id=p.id order by p.id ")
 
 
 records_tuple = cursor.fetchall()
 	
 
-
-wf = open('preemas_csv.csv', 'w')
-csv_writer = csv.writer(wf, delimiter =',', quotechar='"')
-for record in records_tuple:
-	csv_writer.writerow(list(record))
-
+for i in party
+	if i == 0:
+		wf += open('preemas_csv.csv', 'w',newline='')
+		csv_writer = csv.writer(wf, delimiter =',', quotechar='"')
+		for record in records_tuple:
+			csv_writer.writerow(list(record))
+	if i == 1000:
+		wf += open('preemas_csv1.csv', 'w',newline='')
+		csv_writer = csv.writer(wf, delimiter =',', quotechar='"')
+		for record in records_tuple:
+			csv_writer.writerow(list(record))
 wf.close()
